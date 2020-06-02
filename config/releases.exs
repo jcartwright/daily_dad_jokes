00a53b471d0f0821051f34882673eeb6330f294b
Jason Cartwright <jason@ensightenterprises.com>
2020-06-02 11:49:25 -0500

    Fix config/releases.exs to configure DailyDadJokesWeb.Endpoint

--- config/releases.exs
+++ config/releases.exs
@@ -5,6 +5,7 @@ secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
 
 pg_user = System.fetch_env!("POSTGRES_USER")
 pg_pass = System.fetch_env!("POSTGRES_PASSWORD")
+pg_host = System.fetch_env!("POSTGRES_HOST")
 pg_database = System.fetch_env!("POSTGRES_DB")
 
 pg_pool_size =
@@ -26,11 +27,12 @@ port =
 config :daily_dad_jokes, DailyDadJokes.Repo,
   username: pg_user,
   password: pg_pass,
+  hostname: pg_host,
   database: pg_database,
   pool_size: pg_pool_size,
   show_sensitive_data_on_connection_error: false
 
-config :daily_dad_jokes, DailyDadJokes.Endpoint,
+config :daily_dad_jokes, DailyDadJokesWeb.Endpoint,
   http: [port: port],
   secret_key_base: secret_key_base,
   url: [host: {:system, "APP_HOST"}, port: {:system, "PORT"}]
