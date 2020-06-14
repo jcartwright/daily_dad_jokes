3d872c16e554ba4c80f28e631b95851ae15695e1
Jason Cartwright <jason@ensightenterprises.com>
2020-06-14 12:50:33 -0500

    Resolving container naming conflicts

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 # wait for postgres to become available
 while ! pg_isready -h $POSTGRES_HOST
 do
-  echo "Waiting for postgres to accept connections"
+  echo "Waiting for $POSTGRES_HOST to accept connections"
   sleep 2
 done
 
